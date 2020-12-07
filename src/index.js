da57d9ccf830cffb70c53cc574d971d63ac9e434
n-j-m <nick65@gmail.com>
2020-12-07 17:45:57 -0600

    Adds interaction.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const assert = require('assert');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 const Provider = require('oidc-provider');
 
 assert(process.env.HEROKU_APP_NAME, 'process.env.HEROKU_APP_NAME missing');
@@ -10,6 +13,8 @@ assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:
 const jwks = require('./jwks.json');
 
 const RedisAdapter = require('./redis_adapter');
+
+const Account = require('./account');
 
 const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`, {
   adapter: RedisAdapter,
@@ -23,11 +28,21 @@ const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
     }
   ],
   jwks,
+  findAccount: Account.findAccount,
+  claims: {
+    openid: ['sub'],
+    email: ['email', 'email_verified'],
+  },
+  interactions: {
+    url(ctx) {
+      return `/interactions/${ctx.iodc.uid}`;
+    }
+  },
   formats: {
     AccessToken: 'jwt',
   },
   features: {
-    encryption: { enabled: true },
+    devInteractions: { enabled: false },
     introspection: { enabled: true },
     revocation: { enabled: true },
   },
@@ -36,5 +51,98 @@ const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
 oidc.proxy = true;
 oidc.keys = process.env.SECURE_KEY.split(',');
 
+const expressApp = express();
+expressApp.set('trust proxy', true);
+expressApp.set('view engine', 'ejs');
+expressApp.set('views', path.resolve(__dirname, 'views'));
+
+const parse = bodyParser.urlencoded({ extended: false });
+
+function setNoCache(req, res, next) {
+  res.set('Pragma', 'no-cache');
+  res.set('Cache-Control', 'no-cache, no-store');
+  next();
+}
+
+expressApp.get('/interaction/:uid', setNoCache, async (req, res, next) => {
+  try {
+    const details = await oidc.interactionDetails(req, res);
+    console.log('see what else is available to you for interaction views', details);
+    const { uid, prompt, params } = details;
+
+    const client = await oidc.Client.find(params.client_id);
+
+    if (prompt.name === 'login') {
+      return res.render('login', {
+        client,
+        uid,
+        details: prompt.details,
+        params,
+        title: 'Sign-in',
+        flash: undefined,
+      });
+    }
+
+    return res.render('interaction', {
+      client,
+      uid,
+      details: prompt.details,
+      params,
+      title: 'Authorize',
+    });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+expressApp.post('/interaction/:uid/login', setNoCache, parse, async (req, res, next) => {
+  try {
+    const { uid, prompt, params } = await oidc.interactionDetails(req, res);
+    const client = await oidc.Client.find(params.client_id);
+
+    const accountId = await Account.authenticate(req.body.email, req.body.password);
+
+    if (!accountId) {
+      res.render('login', {
+        client,
+        uid,
+        details: prompt.details,
+        params: {
+          ...params,
+          login_hint: req.body.email,
+        },
+        title: 'Sign-in',
+        flash: 'Invalid email or password.',
+      });
+      return;
+    }
+
+    const result = {
+      login: {
+        account: accountId,
+      },
+    };
+
+    await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
+  } catch (err) {
+    nexgt(err);
+  }
+});
+
+expressApp.post('/interaction/:uid/confirm', setNoCach, async (req, res, next) => {
+  try {
+    const result = {
+      erro: 'access_denied',
+      error_description: 'End-Uer aborted interaction',
+    };
+
+    await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
+  } catch (err) {
+    next(err);
+  }
+});
+
+expressApp.use(oidc.callback);
+
 const port = process.env.PORT;
-oidc.listen(port);
+expressApp.listen(port, () => console.log(`running on port ${port}.`));
