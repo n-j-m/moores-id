098b40b7337444a6aeab2ac67d447450bd778cc3
n-j-m <nick65@gmail.com>
2020-12-07 17:06:48 -0600

    Add redis adapter for tokens and sessions

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ assert(process.env.HEROKU_APP_NAME, 'process.env.HEROKU_APP_NAME missing');
 assert(process.env.PORT, 'process.env.PORT missing');
 assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, run `heroku addons:create securekey`');
 assert.strictEqual(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
+assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:hobby-dev`');
 
 const jwks = require('./jwks.json');
+
+const RedisAdapter = require('./redis_adapter');
 
 const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`, {
   clients: [
