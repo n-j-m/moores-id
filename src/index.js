1f39fd723b8c7a00c29ed4e8a4a47a465016abc9
n-j-m <nick65@gmail.com>
2020-12-07 18:19:38 -0600

    Interactions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// see previous example for the things that are not commented
+
 const assert = require('assert');
 const path = require('path');
 const express = require('express');
@@ -7,13 +9,13 @@ const Provider = require('oidc-provider');
 assert(process.env.HEROKU_APP_NAME, 'process.env.HEROKU_APP_NAME missing');
 assert(process.env.PORT, 'process.env.PORT missing');
 assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, run `heroku addons:create securekey`');
-assert.strictEqual(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
+assert.equal(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
 assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:hobby-dev`');
 
+const RedisAdapter = require('./redis_adapter');
 const jwks = require('./jwks.json');
 
-const RedisAdapter = require('./redis_adapter');
-
+// simple account model for this application, user list is defined like so
 const Account = require('./account');
 
 const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`, {
@@ -24,25 +26,36 @@ const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
       redirect_uris: ['https://example.com'],
       response_types: ['id_token'],
       grant_types: ['implicit'],
-      token_endpoint_auth_method: 'none'
-    }
+      token_endpoint_auth_method: 'none',
+    },
   ],
   jwks,
+
+  // oidc-provider only looks up the accounts by their ID when it has to read the claims,
+  // passing it our Account model method is sufficient, it should return a Promise that resolves
+  // with an object with accountId property and a claims method.
   findAccount: Account.findAccount,
+
+  // let's tell oidc-provider you also support the email scope, which will contain email and
+  // email_verified claims
   claims: {
     openid: ['sub'],
     email: ['email', 'email_verified'],
   },
+
+  // let's tell oidc-provider where our own interactions will be
+  // setting a nested route is just good practice so that users
+  // don't run into weird issues with multiple interactions open
+  // at a time.
   interactions: {
     url(ctx) {
-      return `/interactions/${ctx.iodc.uid}`;
-    }
-  },
-  formats: {
-    AccessToken: 'jwt',
+      return `/interaction/${ctx.oidc.uid}`;
+    },
   },
   features: {
+    // disable the packaged interactions
     devInteractions: { enabled: false },
+
     introspection: { enabled: true },
     revocation: { enabled: true },
   },
@@ -51,6 +64,7 @@ const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
 oidc.proxy = true;
 oidc.keys = process.env.SECURE_KEY.split(',');
 
+// let's work with express here, below is just the interaction definition
 const expressApp = express();
 expressApp.set('trust proxy', true);
 expressApp.set('view engine', 'ejs');
@@ -125,24 +139,38 @@ expressApp.post('/interaction/:uid/login', setNoCache, parse, async (req, res, n
 
     await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
   } catch (err) {
-    nexgt(err);
+    next(err);
   }
 });
 
-expressApp.post('/interaction/:uid/confirm', setNoCache, async (req, res, next) => {
+expressApp.post('/interaction/:uid/confirm', setNoCache, parse, async (req, res, next) => {
   try {
     const result = {
-      erro: 'access_denied',
-      error_description: 'End-Uer aborted interaction',
+      consent: {
+        // rejectedScopes: [], // < uncomment and add rejections here
+        // rejectedClaims: [], // < uncomment and add rejections here
+      },
     };
+    await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: true });
+  } catch (err) {
+    next(err);
+  }
+});
 
+expressApp.get('/interaction/:uid/abort', setNoCache, async (req, res, next) => {
+  try {
+    const result = {
+      error: 'access_denied',
+      error_description: 'End-User aborted interaction',
+    };
     await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
   } catch (err) {
     next(err);
   }
 });
 
+// leave the rest of the requests to be handled by oidc-provider, there's a catch all 404 there
 expressApp.use(oidc.callback);
 
-const port = process.env.PORT;
-expressApp.listen(port, () => console.log(`running on port ${port}.`));
+// express listen
+expressApp.listen(process.env.PORT);
